9981af3e80e4724cf166f7a114cf5f480d2b7e40
Leo Claudio <11918571+zllec@users.noreply.github.com>
2025-12-14 10:45:14 +0800

    feat: add healthcheck endpoint

--- main.go
+++ main.go
@@ -1,16 +1,28 @@
 package main
 
 import (
+	"log"
 	"net/http"
 )
 
 func main() {
 	mux := http.ServeMux{}
 
-	mux.Handle("/", http.FileServer(http.Dir(".")))
+	// readiness endpoint
+	// returns 200 OK if the server is ready to accept requests
+	mux.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+		_, err := w.Write([]byte("OK"))
+		if err != nil {
+			log.Fatal(err)
+		}
+	})
 
-	fs := http.FileServer(http.Dir("./assets"))
-	mux.Handle("/assets", http.StripPrefix("/assets", fs))
+	mux.Handle("/app", http.StripPrefix("/app", http.FileServer(http.Dir("./app"))))
+
+	fs := http.FileServer(http.Dir("./app/assets/"))
+	mux.Handle("/app/assets", http.StripPrefix("/app/assets", fs))
 
 	srv := &http.Server{Addr: ":8080", Handler: &mux}
 
